ca7ffd77650cdf7f8ebb1ade37fca5cc6e49f5a0
rona chen <rona.chen@hotmail.com>
2023-05-14 13:25:37 -0400

    added code for storing search history to and retrieve it from the local storgage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,59 @@
-//----------------------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------------------------------------------------------//
+// Global Variables
+//-----------------------------------------------------------------------------------------------------------------------------//
+var APIKey = '187b46953877ee25caa3c164a82a38f2';
 var cityFormEl = document.querySelector('#city-form');
 var cityNameEl = document.querySelector('#city-name');
-var currentWeatherContainerEl = document.querySelector('#current-weather-container');
-var forecastContainerEl = document.querySelector('#forecast-container');
-
-// Submit search form when users enter a valid city name
-function formSubmitHandler(event) {
- event.preventDefault();
-
- var cityName = cityNameEl.value.trim();
-
- if (cityName) {
-    getCityWeather(cityName);
-
-    currentWeatherContainerEl = '';
-    forecastContainerEl = '';
-    cityNameEl.value = '';
- } else {
-    alert('Please enter a valid city!');
- }
+var historyContainerEl = document.querySelector('#history-container');
+//-----------------------------------------------------------------------------------------------------------------------------//
+// Render History: read search history from the local storage or return an empty array ([]) if there aren't any searches
+//-----------------------------------------------------------------------------------------------------------------------------//
+function renderHistoryFromStorage() {
+    searchHistory = localStorage.getItem('searchHistory');
+    if (searchHistory) {
+        searchHistory = JSON.parse(searchHistory);
+    } else {
+        searchHistory = [];
+    }
+    for (var x = 0; x < searchHistory.length; x++) {
+        createHistoryButtons(searchHistory[x]);
+    }
 }
+renderHistoryFromStorage();
+//-----------------------------------------------------------------------------------------------------------------------------//
+// Form Handler: submit search form when users enter a valid city name and save it to local storage
+//-----------------------------------------------------------------------------------------------------------------------------//
+function formSubmitHandler(event) {
+    event.preventDefault();
 
+    var cityName = cityNameEl.value.trim();
+    searchHistory.push(cityName);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    getCityWeather(cityName);
+    createHistoryButtons(cityName);
+    cityNameEl.value = '';
+}
 cityFormEl.addEventListener('submit', formSubmitHandler);
-
-//----------------------------------------------------------------------------------------//
-var APIKey = '187b46953877ee25caa3c164a82a38f2';
-
-function getCityWeather (cityName) {
+//-----------------------------------------------------------------------------------------------------------------------------//
+// Create Buttons: create history buttons after form submission or from reading local storage
+//-----------------------------------------------------------------------------------------------------------------------------//
+function createHistoryButtons(cityName) {
+    var historyButton = document.createElement('button');
+    historyButton.classList.add('btn', 'btn-secondary', 'mb-3');
+    historyButton.setAttribute('type', 'button')
+    historyButton.innerHTML = cityName;
+    historyButton.addEventListener('click', getCityWeather(cityName));
+    historyContainerEl.appendChild(historyButton);
+}
+//-----------------------------------------------------------------------------------------------------------------------------//
+// Retrieve Weather Data:
+//-----------------------------------------------------------------------------------------------------------------------------//
+function getCityWeather(cityName) {
     var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&appid=" + APIKey + '&units=metric';
-    fetch (queryURL)
-    .then(function (response) {
-        response.json();
-    })
+    fetch(queryURL)
+        .then(function (response) {
+            response.json();
+        })
 }
 
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -41,3 +63,4 @@ function getCityWeather (cityName) {
 // list.main.humidity
 // list.wind.speed
 // list.weather.icon
+
