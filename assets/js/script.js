4a41fb49aa0a22eabe1141f4abdd0305695515a6
rona chen <rona.chen@hotmail.com>
2023-05-14 19:41:28 -0400

    changed alert order inside the fetch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,39 +79,38 @@ function weatherFetch(cityName) {
     var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&appid=" + APIKey + '&units=metric';
     fetch(queryURL)
         .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            var unix = data.dt_txt
-            var date = dayjs(unix).format('MMMM D, YYYY');
-            if (data.message === 'city not found') {
-                alert('Please enter an existing city!');    
+            if (response.ok) {
+                response.json().then(function (data) {
+                    var unix = data.dt_txt
+                    var date = dayjs(unix).format('MMMM D, YYYY');
+                    createSearchHistory(data.name);
+                    // Create elements for the current weather container
+                    var cardContainer = document.createElement('div');
+                    var card = document.createElement('div');
+                    var cityTitle = document.createElement('h3');
+                    var icon = document.createElement('img');
+                    var temp = document.createElement('li');
+                    var wind = document.createElement('li');
+                    var hum = document.createElement('li');
+
+                    // Set classess and retrieve icon
+                    cardContainer.setAttribute('class', 'card text-white bg-dark');
+                    card.setAttribute('class', 'card-body');
+                    icon.setAttribute('src', iconURL + data.weather[0].icon + '.png');
+
+                    // Create element text content 
+                    cityTitle.textContent = data.name + ' (' + date + ')';
+                    temp.textContent = 'Temperature: ' + data.main.temp + '°C';
+                    wind.textContent = 'Wind: ' + data.wind.speed + ' m/s';
+                    hum.textContent = 'Humidity: ' + data.main.humidity + '%';
+
+                    // Append the elements
+                    card.append(cityTitle, icon, temp, wind, hum);
+                    cardContainer.append(card);
+                    weatherContainerEl.append(cardContainer);
+                });
             } else {
-                createSearchHistory(data.name);
-                // Create elements for the current weather container
-                var cardContainer = document.createElement('div');
-                var card = document.createElement('div');
-                var cityTitle = document.createElement('h3');
-                var icon = document.createElement('img');
-                var temp = document.createElement('li');
-                var wind = document.createElement('li');
-                var hum = document.createElement('li');
-
-                // Set classess and retrieve icon
-                cardContainer.setAttribute('class', 'card text-white bg-dark'); 
-                card.setAttribute('class', 'card-body');
-                icon.setAttribute('src', iconURL + data.weather[0].icon + '.png');
-
-                // Create element text content 
-                cityTitle.textContent = data.name + ' (' + date + ')';
-                temp.textContent = 'Temperature: ' + data.main.temp + '°C';
-                wind.textContent = 'Wind: ' + data.wind.speed + ' m/s';
-                hum.textContent = 'Humidity: ' + data.main.humidity + '%';
-
-                // Append the elements
-                card.append(cityTitle, icon, temp, wind, hum);
-                cardContainer.append(card);
-                weatherContainerEl.append(cardContainer);
+                alert('Error! Please Try Again!');
             }
         })
 }
@@ -120,63 +119,63 @@ function forcastFetch(cityName) {
     var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + "&appid=" + APIKey + '&units=metric';
     fetch(queryURL)
         .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            if (data.message === 'city not found') {
-                console.log('city does not exist');
+            if (response.ok) {
+                response.json().then(function (data) {
+                    // Create elements for the forecast container
+                    var forecastCardContainer = document.createElement('div');
+                    var forecastCard = document.createElement('div');
+                    var forecastTitle = document.createElement('h3');
+                    var fiveDaysContainer = document.createElement('div');
+
+                    // Add class
+                    forecastCardContainer.setAttribute('class', 'card border-dark');
+                    forecastCard.setAttribute('class', 'card-body');
+                    fiveDaysContainer.setAttribute('class', 'd-flex justify-content-evenly grid row');
+
+                    // Add text content for h3
+                    forecastTitle.textContent = 'Five Day Forecast';
+
+                    // Append on the webpage
+                    forecastCard.append(forecastTitle, fiveDaysContainer);
+                    forecastCardContainer.append(forecastCard);
+                    weatherContainerEl.append(forecastCardContainer);
+
+                    // For loop for creating the elements and data for the 5 day forecast 
+                    for (var x = 0; x < 5; x++) {
+                        var unix = data.list[x].dt_txt
+                        var date = dayjs(unix).format('MMMM D, YYYY');
+                        var dayContainer = document.createElement('div');
+                        var dayBody = document.createElement('div');
+                        var dayTitle = document.createElement('h4');
+                        var dayIcon = document.createElement('img');
+                        var dayTemp = document.createElement('li');
+                        var dayWind = document.createElement('li');
+                        var dayHum = document.createElement('li');
+
+                        // Add class and icon for each day container
+                        dayContainer.setAttribute('class', 'col-2 card');
+                        dayBody.setAttribute('class', 'card-body');
+                        dayIcon.setAttribute('src', iconURL + data.list[x].weather[0].icon + '.png');
+
+                        // Modify text content with data
+                        dayTitle.textContent = ' (' + date + ')';
+                        dayTemp.textContent = 'Temperature: ' + data.list[x].main.temp + '°C';
+                        dayWind.textContent = 'Wind: ' + data.list[x].wind.speed + ' m/s';
+                        dayHum.textContent = 'Humidity: ' + data.list[x].main.humidity + '%';
+
+                        // Append the elements
+                        dayBody.append(dayTitle, dayIcon, dayTemp, dayWind, dayHum);
+                        dayContainer.append(dayBody);
+                        fiveDaysContainer.append(dayContainer);
+                    }
+                });
             } else {
-                console.log(data);
-                // Create elements for the forecast container
-                var forecastCardContainer = document.createElement('div');
-                var forecastCard = document.createElement('div');
-                var forecastTitle = document.createElement('h3');
-                var fiveDaysContainer = document.createElement('div');
-
-                // Add class
-                forecastCardContainer.setAttribute('class', 'card border-dark');
-                forecastCard.setAttribute('class', 'card-body');
-                fiveDaysContainer.setAttribute('class', 'd-flex justify-content-evenly grid row');
-
-                // Add text content for h3
-                forecastTitle.textContent = 'Five Day Forecast';
-
-                // Append on the webpage
-                forecastCard.append(forecastTitle, fiveDaysContainer);
-                forecastCardContainer.append(forecastCard);
-                weatherContainerEl.append(forecastCardContainer);
-                
-                // For loop for creating the elements and data for the 5 day forecast 
-                for (var x = 0; x < 5; x++) {
-                var unix = data.list[x].dt_txt
-                var date = dayjs(unix).format('MMMM D, YYYY');
-                var dayContainer = document.createElement('div');
-                var dayBody = document.createElement('div');
-                var dayTitle = document.createElement('h4');
-                var dayIcon = document.createElement('img');
-                var dayTemp = document.createElement('li');
-                var dayWind = document.createElement('li');
-                var dayHum = document.createElement('li');
-                
-                // Add class and icon for each day container
-                dayContainer.setAttribute('class', 'col-2 card');
-                dayBody.setAttribute('class', 'card-body');
-                dayIcon.setAttribute('src', iconURL + data.list[x].weather[0].icon + '.png');
-                
-                // Modify text content with data
-                dayTitle.textContent = ' (' + date + ')';
-                dayTemp.textContent = 'Temperature: ' + data.list[x].main.temp + '°C';
-                dayWind.textContent = 'Wind: ' + data.list[x].wind.speed + ' m/s';
-                dayHum.textContent = 'Humidity: ' + data.list[x].main.humidity + '%';
-                
-                // Append the elements
-                dayBody.append(dayTitle, dayIcon, dayTemp, dayWind, dayHum);
-                dayContainer.append(dayBody);
-                fiveDaysContainer.append(dayContainer);
-              }
+                console.log('blank input or non-existent city');
             }
         })
+
 }
+
 //-----------------------------------------------------------------------------------------------------------------------------//
 // Event Listeners
 //-----------------------------------------------------------------------------------------------------------------------------//
